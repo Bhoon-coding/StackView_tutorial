24103f3ed750da9791f718267d7ad4b1fbc42372
이병훈 <bhlee@onns.co.kr>
2021-11-11 16:18:08 +0900

    전화기버튼 UI (StackView) 구현

---
+++ StackView_tutorial/CircleBtn.swift
@@ -0,0 +1,23 @@
+//
+//  CircleBtn.swift
+//  StackView_tutorial
+//
+//  Created by BH on 2021/11/11.
+//
+
+import Foundation
+import UIKit
+
+// @IBDesignable: 인터페이스 빌더에서 디자인으로 확인 가능하께끔
+//@IBDesignable
+//class CircleBtn: UIButton {
+//
+//    // @IBInspectable: Inspector 패널에서 사용될 수 있도록 설정
+//    @IBInspectable var cornerRaius: CGFloat = 0 {
+//        didSet {
+//            // UIButton이 가지고있는 layer.cornerRadius에 접근
+//            layer.cornerRadius = cornerRaius
+//            layer.masksToBounds = cornerRaius > 0
+//        }
+//    }
+//}
--- StackView_tutorial/ViewController.swift
+++ StackView_tutorial/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var phoneNumberLabel: UILabel!
+
+    @IBOutlet var phoneNumberButtons: [UIButton]!
+    
+    @IBOutlet var callButton: UIButton!
+    
+    var phoneNumberString = "" {
+        didSet{
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.phoneNumberLabel.textColor = .black
+                self.phoneNumberLabel.text = self.phoneNumberString
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for btnItem in phoneNumberButtons {
+            btnItem.addTarget(self, action: #selector(onNumberBtnClicked(sender:)), for: .touchUpInside)
+        }
+        
+        callButton.addTarget(self, action: #selector(onPhoneCallBtnClicked(_:)), for: .touchUpInside)
     }
 
-
+    @objc fileprivate func onNumberBtnClicked(sender: UIButton) {
+        guard let inputString = sender.titleLabel?.text else { return }
+        phoneNumberString.append(inputString)
+    }
+    
+    @objc fileprivate func onPhoneCallBtnClicked(_ sender: UIButton) {
+        phoneNumberString.removeAll()
+    }
 }
 
